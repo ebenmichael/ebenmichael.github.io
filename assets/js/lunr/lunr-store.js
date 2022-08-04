7666d39adb421ad0b2ed939b971359dcaac4705b
Eli Ben-Michael <ebenmichael@fas.harvard.edu>
2022-08-04 13:44:04 -0400

    Updated Website

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Romania, World War II, and my Grandmother",
+        "excerpt":"I started writing about this story because of my ignorance, because it’s too important to be ignorant about. In passing, I read a mention of the Holocaust in Romania and realized that I knew nothing about it, let alone my own family’s experiences and history involving it. It’s hard to...","categories": [],
+        "tags": [],
+        "url": "/betty-ww2/",
+        "teaser": null
+      }]
